6b088a7fd3088a1fb86d3447252149ec3c77aaf2
Emmanuella <enemiriam522@gmail.com>
2024-05-04 19:22:00 +0100

    fixed bug

--- navbar.js
+++ navbar.js
@@ -1,13 +1,15 @@
-let navigation = document.querySelector('.navigation');
-let closeBtn = document.querySelector('#close');
-let bars = document.querySelector('#bars')
-let blur = document.querySelector('#blur');
+let navigation = document.querySelector(".navigation");
+let closeBtn = document.querySelector("#close");
+let bars = document.querySelector("#bars");
+let blur = document.querySelector("#blur");
 
-bars.addEventListener('click', ()=>{
-    navigation.style.width = '50vw';
-    blur.style.display = 'block'
-})
-closeBtn.addEventListener('click', ()=>{
-    navigation.style.width = '0vw';
-    blur.style.display = 'none'
-})
+bars.addEventListener("click", () => {
+  navigation.style.width = "50vw";
+  navigation.style.backgroundColor = "white";
+  blur.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+  navigation.style.width = "0vw";
+  blur.style.display = "none";
+  navigation.style.backgroundColor = "transparent";
+});
