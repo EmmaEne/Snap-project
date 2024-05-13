1d87c2a1bef308d122ef6c707a58fe69febae8db
Emmanuella <enemiriam522@gmail.com>
2024-05-13 19:00:02 +0100

    second commit

--- navbar.js
+++ navbar.js
@@ -2,9 +2,35 @@ let navigation = document.querySelector(".navigation");
 let closeBtn = document.querySelector("#close");
 let bars = document.querySelector("#bars");
 let blur = document.querySelector("#blur");
-let register = document.querySelector('#signup');
-let login = document.querySelector('#login');
-let registerBoard = document.querySelector('.registerDiv');
+let register = document.querySelector("#signup");
+let login = document.querySelector("#login");
+let registerBoard = document.querySelector(".registerDiv");
+let arrowDown = document.querySelector("#icon-arrow-down");
+let arrow = document.querySelector("#icon-arrow");
+let dropdown = document.querySelectorAll(".dropdown-menu");
+let dropdownBtn = document.querySelectorAll(".dropdown-btn");
+
+dropdownBtn[0].addEventListener("click", myDropdown);
+// dropdownBtn[1].addEventListener('click', myDropdown)
+function myDropdown() {
+  if (arrowDown.classList !== "icon-arrow-up") {
+    arrowDown.classList.toggle("icon-arrow-up");
+    dropdown[0].style.display = "block";
+  }
+  if (arrowDown.classList == "icon-arrow-up") {
+    dropdown[0].style.display = "none";
+  }
+}
+
+dropdownBtn[1].addEventListener('click', ()=>{
+  if (arrow.classList !== "icon-arrow-up") {
+    arrow.classList.toggle("icon-arrow-up");
+    dropdown[1].style.display = "block";
+  }
+  if (arrow.classList == "icon-arrow-up") {
+    dropdown[1].style.display = "none";
+  }
+})
 
 bars.addEventListener("click", () => {
   navigation.style.width = "60vw";
@@ -17,6 +43,13 @@ closeBtn.addEventListener("click", () => {
   navigation.style.backgroundColor = "transparent";
 });
 
-register.addEventListener('click', ()=>{
-  registerBoard.style.visibility = 'visible';
-})
+register.addEventListener("click", () => {
+  registerBoard.style.visibility = "visible";
+});
+
+dropdownBtn[0].addEventListener('click', myDropdown)
+function myDropdown(){
+  dropdown[0].style.display = 'block'
+  arrowDown[0].style.display = 'none'
+  arrowUp[0].style.visibility = 'visible'
+}
